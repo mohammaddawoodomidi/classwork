33fbf651ae7c4b98f22527a78e7bb355eaf450f6
mohammaddawoodomidi <94696652+mohammaddawoodomidi@users.noreply.github.com>
2022-11-10 23:15:14 +0530

    Comments added

--- Insertion_Sort_Linked List.c
+++ Insertion_Sort_Linked List.c
@@ -6,22 +6,22 @@ struct node {
     struct node *link;
 };
 
-void swap(struct node *head, struct node *temp1){
+void swap(struct node *head, struct node *temp1){       // swaps data of two nodes
     int k = head->data;
     head->data = temp1->data;
     temp1->data = k;
 }
 
-void add_at_end( struct node *ptr, int number){
+void add_at_end( struct node *ptr, int number){         // making a linked list by add_at_end approach
     struct node *next = malloc(sizeof(struct node));
     next->data = number;
     next->link = NULL;
     
     struct node *traverse = ptr;
-    while(traverse->link != NULL){
+    while(traverse->link != NULL){      // traverses the linked list until it reaches the last node.
         traverse = traverse->link;
     }
-    traverse->link = next;
+    traverse->link = next;              // connects the newly built node to the last node.
 }
 
 int main(){
@@ -46,14 +46,14 @@ int main(){
     head = temp0;
     temp1 = head->link;
     
-    while(head->link != NULL){
+    while(head->link != NULL){              // this loop traverses the linked list to compare each pair.
         if (head->data > temp1->data){
             swap(head, temp1);
-            head = temp0;
-            temp1 = head->link;
+            head = temp0;                   // when one swap is done, the checking procedure starts over.
+            temp1 = head->link;             // temp1 pointer needs to be updated to stay contiguous with the head pointer.
         }
         else {
-        head = head->link;
+        head = head->link;          // head pointer proceeds further without doing anything and so temp1 pointer. temp1 pointer needs to be one node ahead.
         temp1 = head->link;
         }
     }
