d69ab03a6a8aeba9112b70e10274642b603c701f
mohammaddawoodomidi <94696652+mohammaddawoodomidi@users.noreply.github.com>
2022-11-17 23:48:41 +0530

    Update

--- Heaps_and_Heapsort.c
+++ Heaps_and_Heapsort.c
@@ -1,3 +1,15 @@
 #include <stdio.h>
 
 
+int main(){
+    
+    int s[5] = {101, 101, 36, 64, 25};
+
+    printf("Unsorted numbers : \n");
+    for(int k = 0; k <5; ++k){
+        printf("%d  ", s[k]);
+    }
+    printf("\n");
+    
+}
+
